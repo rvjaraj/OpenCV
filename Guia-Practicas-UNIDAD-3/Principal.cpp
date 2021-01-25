6bfece5f02d70a4c5d48b49b5def1b87c0024674
Ricardo Vinicio Jara Jara <49212955+RicardoVinicioJara@users.noreply.github.com>
2021-01-24 21:46:45 -0500

    todo

--- Guia-Practicas-UNIDAD-3/Principal.cpp
+++ Guia-Practicas-UNIDAD-3/Principal.cpp
@@ -12,34 +12,110 @@
 using namespace std;
 using namespace cv;
 
-Mat imagen;
-int erosion_size = 0;
-int dilation_size = 0;
+Mat imagen1;
+Mat imagen2;
+Mat imagen3;
+Mat imagen4;
+Mat img1;
+Mat img2;
+int erosion_size = 7;
+int dilation_size = 7;
+
+int morph_size = 0;
+int morph_size1 = 0;
+
+/*
+ *Mato para la suma y resta de imagenes
+ * @img1: primera imagen a restar
+ * @img2: Segunda imagen a restar
+ * @img3: imagen que va a ser sumada con el Residuo de la @img1 - @img2
+ * @name: Nombre de la vantana a mostrar la operacion realizada
+ */
+void suma_rest(Mat img1, Mat img2, Mat img3, String name = "Suma | Resta") {
+    Mat resta;
+    Mat suma;
+    absdiff(img1, img2, resta);//Resta
+    addWeighted(img3, 0.5, resta, 0.5, 0, suma);//suma
+    imshow(name, suma);
+}
 
 void Erosion(int, void *) {
-    Mat element = getStructuringElement(MORPH_ELLIPSE, //MORPH_RECT MORPH_CROSS MORPH_ELLIPSE
-                                        Size(2 * erosion_size + 1, 2 * erosion_size + 1));
-    erode(imagen, element, element);
-    imshow("Erosion Demo", element);
+    Mat element = getStructuringElement(MORPH_CROSS, //MORPH_RECT [] MORPH_CROSS + MORPH_ELLIPSE O
+                                        Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+                                        Point(0, 0));
+    erode(imagen1, element, element);
+    imshow("Erosion", element);
 }
 
 
 void Dilation(int, void *) {
-    Mat element = getStructuringElement(MORPH_ELLIPSE, //MORPH_RECT MORPH_CROSS MORPH_ELLIPSE
-                                        Size(2 * dilation_size + 1, 2 * dilation_size + 1));
-    dilate(imagen, element, element);
-    imshow("Dilation Demo", element);
+    Mat element = getStructuringElement(MORPH_ELLIPSE,  //MORPH_RECT [] MORPH_CROSS + MORPH_ELLIPSE O
+                                        Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+                                        Point(0, 0));
+    dilate(imagen2, element, element);
+    imshow("Dilation", element);
 }
 
-int main(int argc, char **argv) {
-    imagen = imread("../LinuxLogo.jpg", IMREAD_COLOR);
-    namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-    namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
+void Morphology_Operations6(int, void *) {
+    int operation = 6;
+    img1 = getStructuringElement(MORPH_ELLIPSE,  //MORPH_RECT [] MORPH_CROSS + MORPH_ELLIPSE O
+                                 Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
+    morphologyEx(imagen2, img1, operation, img1);
+    imshow("Back Hat", img1);
 
-    Erosion(0, 0);
-    Dilation(0, 0);
-    createTrackbar("Kernel 2n +1", "Erosion Demo", &erosion_size, 20,Erosion, nullptr);
-    createTrackbar("Kernel 2n +1", "Dilation Demo", &dilation_size, 20, Dilation, nullptr);
+    suma_rest(img1, img2, imagen4, "Suma | Resta");
+}
+
+void Morphology_Operations5(int, void *) {
+    int operation = 5;
+    img2 = getStructuringElement(MORPH_ELLIPSE,  //MORPH_RECT [] MORPH_CROSS + MORPH_ELLIPSE O
+                                 Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
+    morphologyEx(imagen3, img2, operation, img2);
+    imshow("Top Hat", img2);
+    suma_rest(img1, img2, imagen4, "Suma | Resta");
+
+}
+
+
+int main(int argc, char **argv) {
+
+//    namedWindow("Radiografia 1", WINDOW_AUTOSIZE);
+//    namedWindow("Erosion", WINDOW_AUTOSIZE);
+//    imagen1 = imread("../radio1.jpg", IMREAD_COLOR);
+//    resize(imagen1, imagen1, Size(), 0.4, 0.4);
+//    imshow("Radiografia 1", imagen1);
+//    createTrackbar("Eroicon", "Radiografia 1", &erosion_size, 37, Erosion, nullptr);
+//    Erosion(0, nullptr);
+//
+
+
+
+    namedWindow("Back Hat", WINDOW_AUTOSIZE);
+    imagen2 = imread("../radio1.jpg", IMREAD_COLOR);
+    resize(imagen2, imagen2, Size(), 0.19, 0.19);
+//    Dilation(0, nullptr);
+
+    namedWindow("Radiografia 3", WINDOW_AUTOSIZE);
+    imagen3 = imread("../radio1.jpg", IMREAD_COLOR);
+    resize(imagen3, imagen3, Size(), 0.19, 0.19);
+    imshow("Radiografia 3", imagen3);
+    namedWindow("Top Hat", WINDOW_AUTOSIZE);
+    createTrackbar("Top Hat", "Radiografia 3", &morph_size, 37, Morphology_Operations5);
+    createTrackbar("Back Hat", "Radiografia 3", &morph_size1, 37, Morphology_Operations6);
+
+    img1 = imagen2;
+    img2 = imagen3;
+    imagen4 = imagen3;
+    Morphology_Operations5(0, 0);
+    Morphology_Operations6(0, 0);
+
+
+    namedWindow("Suma | Resta", WINDOW_AUTOSIZE);
+
+
+
+    //
+    //
 
 
     waitKey(0);
