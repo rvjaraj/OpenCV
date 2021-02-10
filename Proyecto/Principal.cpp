bba0de1135b4627350bd6fa41f44f6c83638c827
Ricardo Vinicio Jara Jara <49212955+RicardoVinicioJara@users.noreply.github.com>
2021-02-10 00:49:22 -0500

    saca solo la mano

--- Proyecto/Principal.cpp
+++ Proyecto/Principal.cpp
@@ -42,15 +42,20 @@ int main(int argc, char *argv[]) {
     //VideoCapture video("../video.mp4");
 
     if (video.isOpened()) {
-        Mat bg;
-        Mat frame;
-        Mat roi;
-        Mat roi_gray;
-
         namedWindow("Frame", WINDOW_AUTOSIZE);
         namedWindow("BG", WINDOW_AUTOSIZE);
         namedWindow("ROI", WINDOW_AUTOSIZE);
         namedWindow("ROI_GRAY", WINDOW_AUTOSIZE);
+        namedWindow("RESTA", WINDOW_AUTOSIZE);
+
+        Mat bg;
+        Mat ROI;
+        Mat grayROI;
+        Mat bgROI;
+        Mat roi_bg;
+        Mat frame;
+        Mat dif;
+        Mat th;
         int pixel = 0;
 
         while (3 == 3) {
@@ -60,26 +65,31 @@ int main(int argc, char *argv[]) {
             Rect rect = boundingRect(pointsr);
 
             video >> frame;
-            if (waitKey(23) == 97) {
-                cvtColor(frame, bg, COLOR_BGR2GRAY);
-                bg = bg(rect).clone();
-            }
-
-            if (!bg.empty()) {
-                imshow("BG", bg);
-            }
 
             setMouseCallback("Frame", my_mouse_callback, &frame);
 
             rectangle(frame, a, b, Vec3b(50, 50, 200), 1);
 
 
-            roi = frame(rect).clone();
-            cvtColor(roi, roi_gray, COLOR_BGR2GRAY);
+            if (!bg.empty()) {
+                imshow("BG", roi_bg);
+                ROI = frame(rect).clone();
+                cvtColor(ROI, grayROI, COLOR_BGR2GRAY);
+                bgROI = bg(rect).clone();
+                absdiff(grayROI, bgROI, dif);
+                threshold(dif,th,30,255,THRESH_BINARY);
+                imshow("ROI", ROI);
+                imshow("ROI_GRAY", grayROI);
+                imshow("RESTA", th);
+            }
+
 
             imshow("Frame", frame);
-            imshow("ROI", roi);
-            imshow("ROI_GRAY", roi_gray);
+
+            if (waitKey(23) == 97) {
+                cvtColor(frame, bg, COLOR_BGR2GRAY);
+                roi_bg = bg(rect).clone();
+            }
 
             if (waitKey(23) == 27)
                 break;
